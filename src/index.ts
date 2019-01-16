b883af0b2374c9b1eb8b53eb97d64a3252b3691d
Alex Henry <adhe223@g.uky.edu>
2019-01-15 22:39:24 -0800

    Add sessions

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,26 @@
 require('dotenv').config();
 import * as bodyParser from 'body-parser';
 import * as express from 'express';
+import * as session from 'express-session';
+const MemoryStore = require('memorystore')(session);
 
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.use(
+  session({
+    secret: process.env.sessionSecret,
+    resave: false,
+    saveUninitialized: false,
+    store: new MemoryStore({
+      checkPeriod: 24 * 60 * 60 * 1000, // prune expired entries every 24h
+    }),
+    cookie: {
+      maxAge: 24 * 60 * 60 * 1000,
+      httpOnly: true,
+    },
+  })
+);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
