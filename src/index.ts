b78af56323f076e3521000ee053a327805d69b38
Alex Henry <adhe223@g.uky.edu>
2019-01-15 21:15:24 -0800

    Start rest server

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import * as bodyParser from 'body-parser';
+import * as express from 'express';
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.listen(port);
+
+console.log('RESTful API server started on: ' + port);
+
+// More graceful fallback
+app.use((req, res) => {
+  res.status(404).send({ url: req.originalUrl + ' not found' });
+});
